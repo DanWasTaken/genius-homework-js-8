07b12da660bf90f52dbbd9919d65b79ccebdaf81
DanWasTaken <89984124+DanWasTaken@users.noreply.github.com>
2024-02-15 18:44:14 +0200

    initial Commit

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// Напишіть код, щоб вибрати елемент з атрибутом data-widget-name з документа
+// та прочитати його значення.
+// <!DOCTYPE html>
+// <html>
+// <body>
+// <div data-widget-name="menu">Виберіть жанр</div>
+// <script>/* ваш код */</script>
+// </body>
+// </html>
+
+console.log(document.querySelector('div').getAttribute('data-widget-name'));
